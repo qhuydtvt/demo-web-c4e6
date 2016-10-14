405b547d20cd54dcebed5a8a133c905717df7fb5
qhuydtvt <qhuydtvt@gmail.com>
2016-10-14 20:32:56 +0700

    Fix crash bug when lauching

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from movie import Movie
 from mlab import *
 
 app = Flask(__name__)
+connect(db=db_name, host=host, port=port, username=username, password=password)
 
 @app.route('/')
 def index():
     return render_template("index.html", movies = Movie.objects)
 
 if __name__ == '__main__':
-    connect(db=db_name, host=host, port=port, username=username, password=password)
+
     app.run()
     #
     # movie1 = Movie(title="Terminator", desc="Haha", img_link=TEST_IMG_LINK)
